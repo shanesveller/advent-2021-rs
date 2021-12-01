a36596535f5ae562af57577f523542f4fe1f14d5
Shane Sveller <shanesveller@gmail.com>
2021-12-01 17:09:28 -0600

    feat(solutions): Add day 1 part 2 solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,20 +1,34 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-// use itertools::Itertools;
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u32> {
+pub fn input_generator(input: &str) -> Vec<usize> {
     input
         .lines()
-        .map(str::parse::<u32>)
-        .collect::<Result<Vec<u32>, _>>()
+        .map(str::parse::<usize>)
+        .collect::<Result<Vec<usize>, _>>()
         .unwrap()
 }
 
 #[aoc(day1, part1)]
-pub fn windows(input: &[u32]) -> u32 {
+pub fn windows(input: &[usize]) -> usize {
     input.windows(2).fold(0, |sum, pair| match pair {
         &[prev, next] => {
             if next > prev {
+                sum + 1
+            } else {
+                sum
+            }
+        }
+        _ => unreachable!(),
+    })
+}
+
+#[aoc(day1, part2)]
+pub fn nested_windows(input: &[usize]) -> usize {
+    let triads: Vec<&[usize]> = input.windows(3).collect();
+    triads.windows(2).fold(0, |sum, pair| match pair {
+        &[prev, next] => {
+            if prev.iter().sum::<usize>() < next.iter().sum::<usize>() {
                 sum + 1
             } else {
                 sum
